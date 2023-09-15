8920cbe125e85f69ec685e988dfbc106dd95e0dd
ncardenasfrias <cardenasfrias.natalia@gmail.com>
2023-09-16 00:36:47 +0200

    Initial commit

---
+++ AP 1/AP1.R
@@ -0,0 +1,38 @@
+################################################################################
+# ASSET PRICING 
+# HW 1: Arbitrage Price Theory and risk premium decomposition 
+
+# Miguel Fonseca, Stéphane Eloundou, Natalia Cárdenas 
+# Sept 15th 2023
+################################################################################
+
+# 1. Set up the data base
+
+
+
+
+# 2. Identification of the factors with standard TS procedures
+  
+  # Exogeneous factors: get the unpreductable part (residual) of macroeconomic variables
+
+  # Endogeneous factors 
+
+
+
+
+# 3. Estimation of the beta_{j,k} coefficients (exposure of asset j to risk k) using time series analysis 
+
+  # Check stationarity of the returns 
+
+  # OLS 
+
+
+
+
+# 4. Estimate the market price of each source of systemic risk [cross-sectional analysis]
+
+  # Get empirical mean of the returns of asset j on all the period of observation 
+
+  # Estimation of lambda_{k}
+
+  # Correct noise in last estimation 
