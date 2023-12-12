4832d4456d07e34376628a1f98aabb72690e9d3c
ncardenasfrias <cardenasfrias.natalia@gmail.com>
2023-12-12 11:43:18 +0100

    .

--- FinancialEconometrics1/Backup.R
+++ FinancialEconometrics1/Backup.R
@@ -11,7 +11,7 @@ setwd('/Users/nataliacardenasf/Documents/GitHub/PROJECTS_AP_FE/FinancialEconomet
 ###########################
 
 #Load the dataset, data gathering and cleaning done in Python
-df <- fread("DATA/data.csv")
+df = fread("DATA/data.csv")
 as.data.table(df)
 #change few names of columns to got something more clear 
 names(df)[1] = 'Date'
@@ -39,13 +39,13 @@ allts[['Date']] = NULL # R was making regressions on the date column :/
 allts[['manufacturing']] = NULL # rather not have data that was initially yearly
 allts[['rpi']] = NULL # redundent with dpi 
 allts[['unempl']] = NULL # did not lead anywhere 
-
+allts[['corp_debt']] = NULL # we end up not using it and the UR test are weird
 
 
 #Reorganise the list 
-desired_order = c('gdp', 'dpi', 'infl_e', 'deflator', 'rate', 'splong', 'corp_debt')
+desired_order = c('gdp', 'dpi', 'infl_e', 'deflator', 'rate', 'splong')
 allts = allts[desired_order]
-names = c('GDP', "Disposable Income", 'Inflation expectation', 'PIB deflator', 'Fed rate', 'SP500', 'Corporate debt rate')
+names = c('GDP', "Disposable Income", 'Inflation expectation', 'PIB deflator', 'Fed rate', 'SP500')
 rm(df, desired_order)
 
 #Remove outliers => IT DOES NOT CHANGE ANYTHING
@@ -63,16 +63,15 @@ rm(df, desired_order)
 #################################
 
 ### ADF 1st regression: deterministic trend + drift 
-results_adf_trend <- list()
+results_adf_trend = list()
 for (var_name in names(allts)) {
-  result <- ur.df(allts[[var_name]], type = "trend", selectlags = "BIC")
-  results_adf_trend[[var_name]] <- summary(result)
+  result = ur.df(allts[[var_name]], type = "trend", selectlags = "BIC")
+  results_adf_trend[[var_name]] = summary(result)
 }
 #export summary adf
 trend_test =cbind(t(results_adf_trend$gdp@teststat), t(results_adf_trend$dpi@teststat),
                   t(results_adf_trend$infl_e@teststat),t(results_adf_trend$deflator@teststat), 
-                  t(results_adf_trend$rate@teststat),
-                  t(results_adf_trend$splong@teststat), t(results_adf_trend$corp_debt@teststat),
+                  t(results_adf_trend$rate@teststat), t(results_adf_trend$splong@teststat),
                   results_adf_trend$infl_e@cval)
 colnames(trend_test) = c(names(results_adf_trend), "CV 1pct", "CV 5pct", "CV 10pct")
 stargazer(trend_test, type='text')
@@ -87,7 +86,7 @@ for (i in names(results_adf_trend)){
   j = results_adf_trend[[i]]@testreg$coefficients[,'t value']
   tab = as.data.frame(j)
   row = c(tab$j[1], tab$j[2], tab$j[3], tab$j[4])
-  t_values_table[nrow(t_values_table) + 1,] <- row
+  t_values_table[nrow(t_values_table) + 1,] = row
 }
 row.names(t_values_table) = names(results_adf_trend) # call the rows as the series
 t_values_table = t(t_values_table) #transpose the table, looks better
@@ -96,6 +95,8 @@ stargazer(t_values_table,type='text') #all ok
 #         notes = '\\footnotesize Notes: With N=396, critical values at 5\\%: alpha = 3.09 ; gamma= -3.43 ; beta = 2.79',
 #        label='tab:tstat_trend')
 
+
+###################### stopped here
 #extract series that need to keep being tested
 adf_v2 = allts[c(1:2, 5:7)]
 names(adf_v2) = names(allts)[c(1:2, 5:7)]
@@ -103,10 +104,10 @@ names(adf_v2) = names(allts)[c(1:2, 5:7)]
 
 
 ### ADF 2nd regression: drift
-results_adf_drift <- list()
+results_adf_drift = list()
 for (var_name in names(adf_v2)) {
-  result <- ur.df(adf_v2[[var_name]], type = "drif", selectlags = "BIC")
-  results_adf_drift[[var_name]] <- summary(result)
+  result = ur.df(adf_v2[[var_name]], type = "drif", selectlags = "BIC")
+  results_adf_drift[[var_name]] = summary(result)
 }
 #export summary adf
 drift_test =cbind(t(results_adf_drift$gdp@teststat), t(results_adf_drift$dpi@teststat),
@@ -125,7 +126,7 @@ for (i in names(results_adf_drift)){
   j = results_adf_drift[[i]]@testreg$coefficients[,'t value']
   tab = as.data.frame(j)
   row = c(tab$j[1], tab$j[2], tab$j[3])
-  t_values_table2[nrow(t_values_table2) + 1,] <- row
+  t_values_table2[nrow(t_values_table2) + 1,] = row
 }
 row.names(t_values_table2) = names(results_adf_drift) # call the rows as the series
 t_values_table2 = t(t_values_table2) #transpose the table, looks better
@@ -139,10 +140,10 @@ names(adf_v3) = names(adf_v2[1:4])
 
 
 ### ADF 3rd regression: UR only
-results_adf_none <- list()
+results_adf_none = list()
 for (var_name in names(adf_v3)) {
-  result <- ur.df(adf_v3[[var_name]], type = "none", selectlags = "BIC")
-  results_adf_none[[var_name]] <- summary(result)
+  result = ur.df(adf_v3[[var_name]], type = "none", selectlags = "BIC")
+  results_adf_none[[var_name]] = summary(result)
 }
 #export summary adf
 none_test =cbind(t(results_adf_none$gdp@teststat), 
@@ -162,7 +163,7 @@ for (i in names(results_adf_none)){
   j = results_adf_none[[i]]@testreg$coefficients[,'t value']
   tab = as.data.frame(j)
   row = c(tab$j[1], tab$j[2], tab$j[3])
-  t_values_table3[nrow(t_values_table3) + 1,] <- row
+  t_values_table3[nrow(t_values_table3) + 1,] = row
 }
 row.names(t_values_table3) = names(results_adf_none) # call the rows as the series
 t_values_table3 = t(t_values_table3) #transpose the table, looks better
@@ -179,10 +180,10 @@ deltas = list (diff(allts$gdp), diff(allts$dpi),
 names(deltas) = list("d_gdp","d_dpi","d_rate", "d_splong")
 
 ### CHECK THAT DELTAS ARE I(0), I am using same code as in levels 
-results_adf_trend <- list()
+results_adf_trend = list()
 for (var_name in names(deltas)) {
-  result <- ur.df(deltas[[var_name]], type = "trend", selectlags = "BIC")
-  results_adf_trend[[var_name]] <- summary(result)
+  result = ur.df(deltas[[var_name]], type = "trend", selectlags = "BIC")
+  results_adf_trend[[var_name]] = summary(result)
 }
 #export summary adf
 trend_deltas =cbind(t(results_adf_trend$d_gdp@teststat), #t(results_adf_trend$d_rpi@teststat),
@@ -228,19 +229,19 @@ decom_i = grid.arrange(dec_graphs[[1]],dec_graphs[[2]], dec_graphs[[3]], ncol=3)
 
 ### Run OLS regression on each component of the TS
 #create a df with all the series and the monthly dummies
-start_date <- as.Date("1990-01-01")
-end_date <- as.Date("2022-12-01") # Assuming December 2022
-all_dates <- seq(start_date, end_date, by = "month")
+start_date = as.Date("1990-01-01")
+end_date = as.Date("2022-12-01") # Assuming December 2022
+all_dates = seq(start_date, end_date, by = "month")
 
-df_i0level <- data.frame(Date = all_dates)
+df_i0level = data.frame(Date = all_dates)
 for (i in seq_along(i0_levels)) {
-  col_name <- paste0("Series_", i)
-  df_i0level[[col_name]] <- i0_levels[[i]]
+  col_name = paste0("Series_", i)
+  df_i0level[[col_name]] = i0_levels[[i]]
 }
 colnames(df_i0level) = c('Date', names(i0_levels))
 rownames(df_i0level) = df_i0level$Date #date as index
-df_i0level$MONTH<-month(df_i0level$Date) #Get month dummies 
-df_i0level[paste0("M", 1:12)] <- as.data.frame(t(sapply(df_i0level$MONTH, tabulate, 12)))
+df_i0level$MONTH=month(df_i0level$Date) #Get month dummies 
+df_i0level[paste0("M", 1:12)] = as.data.frame(t(sapply(df_i0level$MONTH, tabulate, 12)))
 
 df_i0level$Date = NULL #no more need date column
 df_i0level$MONTH = NULL #no more need MONTH column
@@ -257,23 +258,96 @@ stargazer(infl_e_ols_dec,deflator_ols_dec,corp_debt_ols_dec, type='text')
 #           type='latex', out="TABLES/ols_decomp_levels", label='tab:ols_dec_levels',
 #           title='OLS decomposition of I(0) series' )
 
-## remove drifts and store in new list
+
+## remove drifts and trend and store in new list
 i0_levels_no_drift = list()
 
-ct_infl <- coef(infl_e_ols_dec)[1]  
-i0_levels_no_drift$infl_e <- i0_levels$infl_e - ct_infl
-ct_defl <- coef(deflator_ols_dec)[1]  
-i0_levels_no_drift$deflator <- i0_levels$deflator - ct_defl
-ct_corp <- coef(corp_debt_ols_dec)[1]  
-i0_levels_no_drift$corp_debt <- i0_levels$corp_debt - ct_corp
+ct_infl = coef(infl_e_ols_dec)[1]  
+trend_infl = coef(infl_e_ols_dec)[2] 
+m5_infl = coef(infl_e_ols_dec)[7]
+i0_levels_no_drift$infl_e = i0_levels$infl_e - ct_infl - (df_i0level$trend*trend_infl) - (df_i0level$M5*m5_infl)
+
+ct_defl = coef(deflator_ols_dec)[1]  
+trend_defl = coef(deflator_ols_dec)[2]  
+i0_levels_no_drift$deflator = i0_levels$deflator - ct_defl- (df_i0level$trend*trend_defl)
+
+ct_corp = coef(corp_debt_ols_dec)[1]  
+trend_corp = coef(corp_debt_ols_dec)[2]  
+i0_levels_no_drift$corp_debt = i0_levels$corp_debt - ct_corp - (df_i0level$trend*trend_corp)
+
+
 
 
 ### Find p and q with PACF and ACF                         
-acf(i0_levels_no_drift$infl_e)
 
-
-Acf(i0_levels_no_drift$infl_e, lag.max = 20, type = "correlation", plot = TRUE, na.action = na.contiguous, demean = TRUE)
+Acf(i0_levels_no_drift$infl_e, lag.max = 20, type = "correlation", plot = TRUE, na.action = na.contiguous, demean = TRUE, title = "ACF Inflation Expectation")
 Pacf(i0_levels_no_drift$infl_e, lag.max = 20, plot = TRUE, na.action = na.contiguous, demean = TRUE)
+
+acf_infl = ggAcf(i0_levels_no_drift$infl_e, lag.max= 40) + labs(title = 'ACF - infl_e')
+acf_defl= ggAcf(i0_levels_no_drift$deflator, lag.max= 40) + labs(title = 'ACF - deflator')
+acf_corp = ggAcf(i0_levels_no_drift$corp_debt, lag.max= 70) + labs(title = 'ACF - corp_debt')
+
+ggsave('IMAGES/acf_infl.png', plot=acf_infl, width = 12, height = 8)
+ggsave('IMAGES/acf_defl.png', plot=acf_defl, width = 12, height = 8)
+ggsave('IMAGES/acf_corp.png', plot=acf_corp, width = 12, height = 8)
+
+pacf_infl = ggPacf(i0_levels_no_drift$infl_e)+ labs(title = 'PACF - infl_e')
+pacf_defl = ggPacf(i0_levels_no_drift$deflator) + labs(title = 'PACF - deflator')
+pacf_corp = ggPacf(i0_levels_no_drift$corp_debt) + labs(title = 'PACF - corp_debt')
+
+ggsave('IMAGES/pacf_infl.png', plot= pacf_infl, width = 12, height=8)
+ggsave('IMAGES/pacf_defl.png', plot= pacf_defl, width = 12, height=8)
+ggsave('IMAGES/pacf_corp.png', plot= pacf_corp, width = 12, height=8)
+
+
+
+## create a function to get best combination of parameters 
+
+find_best_ARMA = function(ts_data, max_ar_order, max_ma_order) {
+  best_model_aic = NULL
+  best_model_bic = NULL
+  best_aic = Inf
+  best_bic = Inf
+  for (p in 0:max_ar_order) {
+    for (q in 0:max_ma_order) {
+      # Skip models where both AR and MA orders are 0
+      if (p == 0 & q == 0) next
+      # Fit ARMA model
+      arma_model = arima(ts_data, order = c(p, 0, q))
+      # Calculate AIC and BIC
+      aic = AIC(arma_model)
+      bic = BIC(arma_model)
+      # Update best model based on AIC
+      if (aic < best_aic) {
+        best_aic = aic
+        best_model_aic = arma_model
+      }
+      # # Update best model based on BIC
+      # if (bic < best_bic) {
+      #   best_bic = bic
+      #   best_model_bic = arma_model
+      # }
+    }
+  }
+  
+  # Return best models based on AIC and BIC
+  return(list(best_model_aic = best_model_aic))#, best_model_bic = best_model_bic))
+}
+
+
+#use it 
+best_infl = find_best_ARMA(i0_levels_no_drift$infl_e, 13, 15)
+print("Best model based on AIC:")
+print(best_infl$best_model_aic)
+
+print("Best model based on BIC:")
+print(best_infl$best_model_bic)
+
+
+
+
+
+
 
 
 #################################
@@ -305,19 +379,19 @@ decom_ii = grid.arrange(dec_graphs_d[[1]],dec_graphs_d[[2]], dec_graphs_d[[3]],d
 
 ### Run OLS regression on each component of the TS
 #create a df with all the series and the monthly dummies
-start_date <- as.Date("1990-02-01")
-end_date <- as.Date("2022-12-01") 
-all_dates <- seq(start_date, end_date, by = "month")
+start_date = as.Date("1990-02-01")
+end_date = as.Date("2022-12-01") 
+all_dates = seq(start_date, end_date, by = "month")
 
-df_deltas <- data.frame(Date = all_dates)
+df_deltas = data.frame(Date = all_dates)
 for (i in seq_along(deltas)) {
-  col_name <- paste0("Series_", i)
-  df_deltas[[col_name]] <- deltas[[i]]
+  col_name = paste0("Series_", i)
+  df_deltas[[col_name]] = deltas[[i]]
 }
 colnames(df_deltas) = c('Date', names(deltas))
 rownames(df_deltas) = df_deltas$Date #date as index
-df_deltas$MONTH<-month(df_deltas$Date) #Get month dummies 
-df_deltas[paste0("M", 1:12)] <- as.data.frame(t(sapply(df_deltas$MONTH, tabulate, 12)))
+df_deltas$MONTH=month(df_deltas$Date) #Get month dummies 
+df_deltas[paste0("M", 1:12)] = as.data.frame(t(sapply(df_deltas$MONTH, tabulate, 12)))
 
 df_deltas$Date = NULL #no more need date column
 df_deltas$MONTH = NULL #no more need MONTH column
@@ -338,14 +412,14 @@ stargazer(d_gdp_ols_dec,d_dpi_ols_dec,d_rate_ols_dec, d_splong_ols_dec, type='te
 ## remove drifts and store in new list (remove trend for rate only)
 deltas_no_drift_trend = list()
 
-ct_gdp <- coef(d_gdp_ols_dec)[1]  
+ct_gdp = coef(d_gdp_ols_dec)[1]  
 deltas_no_drift_trend$d_gdp = deltas$d_gdp - ct_gdp
-ct_dpi <- coef(d_dpi_ols_dec)[1]  
+ct_dpi = coef(d_dpi_ols_dec)[1]  
 deltas_no_drift_trend$d_dpi = deltas$d_dpi - ct_dpi
-ct_rate <- coef(d_rate_ols_dec)[1]  
-trend_rate <- coef(d_rate_ols_dec)[2]
+ct_rate = coef(d_rate_ols_dec)[1]  
+trend_rate = coef(d_rate_ols_dec)[2]
 deltas_no_drift_trend$d_rate = deltas$d_rate - ct_rate - (df_deltas$trend * trend_rate)
-ct_sp <- coef(d_splong_ols_dec)[1] 
+ct_sp = coef(d_splong_ols_dec)[1] 
 deltas_no_drift_trend$d_splong = deltas$d_splong - ct_sp
 
 
@@ -364,7 +438,7 @@ levels_var = data.frame(
   gdp = allts$gdp,
   dpi = allts$dpi) 
 
-#levels_var$timestamp <- time(deseasonalized_ts$deflator) 
+#levels_var$timestamp = time(deseasonalized_ts$deflator) 
 names_level = c('splong', 'rate', 'gdp', 'dpi')
 
 lag_order = VARselect(levels_var)
